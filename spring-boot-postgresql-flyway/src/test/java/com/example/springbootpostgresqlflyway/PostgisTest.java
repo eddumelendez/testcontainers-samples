6ac590fc5404e5b864c3d51630e1de61a7534c11
Eddú Meléndez <eddu.melendez@gmail.com>
2022-08-01 14:03:51 -0500

    Add samples with postgis and timescaledb

---
+++ spring-boot-postgresql-flyway/src/test/java/com/example/springbootpostgresqlflyway/PostgisTest.java
@@ -0,0 +1,45 @@
+package com.example.springbootpostgresqlflyway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Testcontainers
+@JdbcTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class PostgisTest {
+
+    private static final DockerImageName dockerImageName = DockerImageName.parse("postgis/postgis")
+            .withTag("14-3.2-alpine")
+            .asCompatibleSubstituteFor("postgres");
+
+    @Container
+    private static final PostgreSQLContainer postgres = new PostgreSQLContainer(dockerImageName);
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @DynamicPropertySource
+    static void sqlserverProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+    @Test
+    void test() {
+        var records = this.jdbcTemplate.queryForList("select * from profile");
+        assertThat(records).hasSize(1);
+    }
+
+}
---
+++ spring-boot-postgresql-flyway/src/test/java/com/example/springbootpostgresqlflyway/TimescaleTest.java
@@ -0,0 +1,46 @@
+package com.example.springbootpostgresqlflyway;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Testcontainers
+@JdbcTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class TimescaleTest {
+
+    private static final DockerImageName dockerImageName = DockerImageName.parse("timescale/timescaledb")
+            .withTag("2.7.2-pg14")
+            .asCompatibleSubstituteFor("postgres");
+
+    @Container
+    private static final PostgreSQLContainer postgres = new PostgreSQLContainer(dockerImageName);
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @DynamicPropertySource
+    static void sqlserverProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+    @Test
+    void test() {
+        var records = this.jdbcTemplate.queryForList("select * from profile");
+        assertThat(records).hasSize(1);
+    }
+
+}
