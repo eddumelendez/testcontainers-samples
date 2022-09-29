85a3bb27f34c8f0f7bdb62cc88f35566d08c9d33
Eddú Meléndez <eddu.melendez@gmail.com>
2022-09-29 13:00:05 -0500

    Add sample with tidb

---
+++ spring-boot-tidb-liquibase/src/main/java/com/example/springboottidbliquibase/SpringBootTiDBLiquibaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboottidbliquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTiDBLiquibaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTiDBLiquibaseApplication.class, args);
+    }
+
+}
---
+++ spring-boot-tidb-liquibase/src/test/java/com/example/springboottidbliquibase/TiDBTest.java
@@ -0,0 +1,40 @@
+package com.example.springboottidbliquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.tidb.TiDBContainer;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Testcontainers
+@DataJdbcTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class TiDBTest {
+
+    @Container
+    private static final TiDBContainer postgres = new TiDBContainer("pingcap/tidb:v6.1.0");
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @DynamicPropertySource
+    static void sqlserverProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+    @Test
+    void test() {
+        var records = this.jdbcTemplate.queryForList("select count(*) from profile");
+        assertThat(records).hasSize(1);
+    }
+
+}
