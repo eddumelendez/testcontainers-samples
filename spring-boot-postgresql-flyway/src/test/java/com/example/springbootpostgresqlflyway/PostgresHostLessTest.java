c74fa2cb9c779b0dc6c15ae7e460ab9fbc20fa6d
Eddú Meléndez <eddu.melendez@gmail.com>
2022-10-15 13:21:02 -0500

    Add tests for postgres 15

---
+++ spring-boot-postgresql-flyway/src/test/java/com/example/springbootpostgresqlflyway/PostgresHostLessTest.java
@@ -0,0 +1,25 @@
+package com.example.springbootpostgresqlflyway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Testcontainers
+@DataJdbcTest(properties = {"spring.datasource.url=jdbc:tc:postgresql:15-alpine:///"})
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class PostgresHostLessTest {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Test
+    void test() {
+        var records = this.jdbcTemplate.queryForList("select count(*) from profile");
+        assertThat(records).hasSize(1);
+    }
+}
---
+++ spring-boot-postgresql-flyway/src/test/java/com/example/springbootpostgresqlflyway/PostgresTest.java
@@ -0,0 +1,40 @@
+package com.example.springbootpostgresqlflyway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Testcontainers
+@JdbcTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class PostgresTest {
+
+    @Container
+    private static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15-alpine");
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @DynamicPropertySource
+    static void sqlserverProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+    @Test
+    void test() {
+        var records = this.jdbcTemplate.queryForList("select * from profile");
+        assertThat(records).hasSize(1);
+    }
+
+}
