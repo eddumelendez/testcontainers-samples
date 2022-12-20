80f5273f0907267ffedb530e11702478b33ec87e
Eddú Meléndez <eddu.melendez@gmail.com>
2022-12-19 18:11:46 -0600

    Add example with yugabytedb and flyway

---
+++ spring-boot-yugabytedb-flyway/src/main/java/com/example/springbootyugabytedbflyway/SpringBootYugabyteDbFlywayApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootyugabytedbflyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootYugabyteDbFlywayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootYugabyteDbFlywayApplication.class, args);
+	}
+
+}
---
+++ spring-boot-yugabytedb-flyway/src/test/java/com/example/springbootyugabytedbflyway/YugabyteDbHostLessTest.java
@@ -0,0 +1,26 @@
+package com.example.springbootyugabytedbflyway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Testcontainers
+@DataJdbcTest(properties = { "spring.datasource.url=jdbc:tc:yugabyte:2.14.4.0-b26:///" })
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class YugabyteDbHostLessTest {
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	@Test
+	void test() {
+		var records = this.jdbcTemplate.queryForList("select count(*) from profile");
+		assertThat(records).hasSize(1);
+	}
+
+}
