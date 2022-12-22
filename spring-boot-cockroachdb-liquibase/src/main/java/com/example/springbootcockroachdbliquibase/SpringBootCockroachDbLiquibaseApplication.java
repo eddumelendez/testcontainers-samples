91a4592b4dd186797007d5f5d2ae96e2b496e0d0
Eddú Meléndez <eddu.melendez@gmail.com>
2022-12-21 23:58:36 -0600

    Add example with cockroachdb and liquibase

---
+++ spring-boot-cockroachdb-liquibase/src/main/java/com/example/springbootcockroachdbliquibase/SpringBootCockroachDbLiquibaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootcockroachdbliquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCockroachDbLiquibaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCockroachDbLiquibaseApplication.class, args);
+	}
+
+}
