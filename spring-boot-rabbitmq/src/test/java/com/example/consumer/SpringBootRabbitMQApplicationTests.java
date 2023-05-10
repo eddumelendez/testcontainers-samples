d51ca7b96ed60eedb014e3965ee9be53d8f7e6c3
Eddú Meléndez <eddu.melendez@gmail.com>
2023-05-09 19:20:42 -0600

    Add spring-boot-rabbitmq

---
+++ spring-boot-rabbitmq/src/test/java/com/example/consumer/SpringBootRabbitMQApplicationTests.java
@@ -0,0 +1,67 @@
+package com.example.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.RabbitMQContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.waitAtMost;
+
+@SpringBootTest
+@Testcontainers
+class SpringBootRabbitMQApplicationTests {
+
+	@Container
+	@ServiceConnection
+	static RabbitMQContainer rabbitmq = new RabbitMQContainer("rabbitmq:3.11-alpine");
+
+	@Autowired
+	private AmqpTemplate amqpTemplate;
+
+	@Autowired
+	private TestListener testListener;
+
+	@Test
+	void consumeMessage() {
+		this.amqpTemplate.convertAndSend("test", "test-data");
+
+		waitAtMost(Duration.ofSeconds(30)).untilAsserted(() -> {
+			assertThat(this.testListener.messages).hasSize(1);
+		});
+	}
+
+	@TestConfiguration
+	static class Config {
+
+		@Bean
+		TestListener testListener() {
+			return new TestListener();
+		}
+
+	}
+
+	static class TestListener {
+
+		private final List<String> messages = new ArrayList<>();
+
+		@RabbitListener(queuesToDeclare = @Queue("test"))
+		void listen(String data) {
+			this.messages.add(data);
+		}
+
+	}
+
+}
