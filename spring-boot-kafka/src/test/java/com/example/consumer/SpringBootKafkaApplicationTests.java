c775a75f1f935ed4faa3559ce82e520bb22efa43
Eddú Meléndez <eddu.melendez@gmail.com>
2023-07-22 23:35:04 -0600

    Polish

--- spring-boot-kafka/src/test/java/com/example/consumer/SpringBootKafkaApplicationTests.java
+++ spring-boot-kafka/src/test/java/com/example/consumer/SpringBootKafkaApplicationTests.java
@@ -1,6 +1,5 @@
 package com.example.consumer;
 
-import junit.framework.TestListener;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
--- spring-boot-kafka/src/test/java/com/example/consumer/SpringBootRedpandaApplicationTests.java
+++ spring-boot-kafka/src/test/java/com/example/consumer/SpringBootRedpandaApplicationTests.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.test.context.TestPropertySource;
-import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.redpanda.RedpandaContainer;
