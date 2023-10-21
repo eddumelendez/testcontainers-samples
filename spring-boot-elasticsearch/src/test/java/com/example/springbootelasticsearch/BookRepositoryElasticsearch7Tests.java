feea40a0bb87d1c3faf832a68bdacee6320f7673
Eddú Meléndez <eddu.melendez@gmail.com>
2023-10-20 22:40:11 -0600

    Polish

--- spring-boot-elasticsearch/src/test/java/com/example/springbootelasticsearch/BookRepositoryElasticsearch7Tests.java
+++ spring-boot-elasticsearch/src/test/java/com/example/springbootelasticsearch/BookRepositoryElasticsearch7Tests.java
@@ -1,6 +1,5 @@
 package com.example.springbootelasticsearch;
 
-import org.assertj.core.api.InstanceOfAssertFactories;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.elasticsearch.DataElasticsearchTest;
