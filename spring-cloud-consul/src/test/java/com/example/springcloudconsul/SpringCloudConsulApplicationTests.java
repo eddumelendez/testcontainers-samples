a9b3e55d175b572a65e14d3c02e25f2c5796e76e
Eddú Meléndez <eddu.melendez@gmail.com>
2023-11-01 23:30:05 -0600

    Use ConfigDataApplicationContextInitializer in consul example

--- spring-cloud-consul/src/test/java/com/example/springcloudconsul/SpringCloudConsulApplicationTests.java
+++ spring-cloud-consul/src/test/java/com/example/springcloudconsul/SpringCloudConsulApplicationTests.java
@@ -1,10 +1,13 @@
 package com.example.springcloudconsul;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.consul.ConsulContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -12,8 +15,8 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.containsString;
 
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT,
-		properties = { "spring.config.import=consul:", "spring.cloud.consul.config.name=tc" })
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, properties = { "spring.cloud.consul.config.name=tc" })
+@ContextConfiguration(initializers = ConfigDataApplicationContextInitializer.class)
 @Testcontainers
 class SpringCloudConsulApplicationTests {
 
@@ -24,13 +27,12 @@ class SpringCloudConsulApplicationTests {
 	private static ConsulContainer consul = new ConsulContainer("hashicorp/consul:1.13.2")
 		.withConsulCommand("kv put config/tc/message Hello");
 
-	@BeforeAll
-	static void beforeAll() {
-		System.setProperty("spring.cloud.consul.host", consul.getHost());
-		System.setProperty("spring.cloud.consul.port", String.valueOf(consul.getFirstMappedPort()));
-		// or
-		// System.setProperty("spring.config.import=", String.format("consul:%s:%d",
-		// consul.getHost(), consul.getFirstMappedPort()));
+	@DynamicPropertySource
+	static void properties(DynamicPropertyRegistry registry) {
+		registry.add("spring.cloud.consul.host", consul::getHost);
+		registry.add("spring.cloud.consul.port", consul::getFirstMappedPort);
+		registry.add("spring.config.import",
+				() -> "consul:%s:%d".formatted(consul.getHost(), consul.getFirstMappedPort()));
 	}
 
 	@Test
