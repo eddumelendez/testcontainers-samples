c66cd66ecddbdf692b917d48ea68978c6fe2b012
Eddú Meléndez <eddu.melendez@gmail.com>
2023-11-14 21:13:48 -0600

    Add spring-boot-oracle

---
+++ spring-boot-oracle/src/main/java/org/example/springbootoracle/SpringBootOracleApplication.java
@@ -0,0 +1,13 @@
+package org.example.springbootoracle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOracleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOracleApplication.class, args);
+	}
+
+}
---
+++ spring-boot-oracle/src/test/java/org/example/springbootoracle/SpringBootOracleApplicationTests.java
@@ -0,0 +1,31 @@
+package org.example.springbootoracle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.jdbc.core.simple.JdbcClient;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.oracle.OracleContainer;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Testcontainers
+class SpringBootOracleApplicationTests {
+
+	@Container
+	@ServiceConnection
+	static OracleContainer oracle = new OracleContainer("gvenzl/oracle-free:23.3-slim-faststart");
+
+	@Autowired
+	private JdbcClient jdbcClient;
+
+	@Test
+	void test() {
+		var records = this.jdbcClient.sql("select * from profile").query().listOfRows();
+		assertThat(records).hasSize(1);
+	}
+
+}
