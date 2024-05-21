e97f4e89e4e5f2b9c708a36b01f52545018fb6e2
Eddú Meléndez <eddu.melendez@gmail.com>
2024-05-21 09:27:40 -0600

    Add examples using apache/kafka image

---
+++ spring-boot-kafka/src/test/java/com/example/consumer/SpringBootApacheKafkaApplicationTests.java
@@ -0,0 +1,73 @@
+package com.example.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.TestPropertySource;
+import org.testcontainers.kafka.KafkaContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.waitAtMost;
+
+@SpringBootTest
+@Testcontainers
+@TestPropertySource(properties = "spring.kafka.consumer.auto-offset-reset=earliest")
+class SpringBootApacheKafkaApplicationTests {
+
+	@Container
+	static KafkaContainer kafka = new KafkaContainer("apache/kafka:3.7.0");
+
+	@DynamicPropertySource
+	static void properties(DynamicPropertyRegistry registry) {
+		registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers);
+	}
+
+	@Autowired
+	private KafkaTemplate<String, String> kafkaTemplate;
+
+	@Autowired
+	private TestListener testListener;
+
+	@Test
+	void consumeMessage() {
+		this.kafkaTemplate.send("test", "test-data");
+
+		waitAtMost(Duration.ofSeconds(30)).untilAsserted(() -> {
+			assertThat(this.testListener.messages).hasSize(1);
+		});
+	}
+
+	@TestConfiguration
+	static class Config {
+
+		@Bean
+		TestListener testListener() {
+			return new TestListener();
+		}
+
+	}
+
+	static class TestListener {
+
+		private final List<String> messages = new ArrayList<>();
+
+		@KafkaListener(topics = "test", groupId = "test")
+		void listen(String data) {
+			this.messages.add(data);
+		}
+
+	}
+
+}
