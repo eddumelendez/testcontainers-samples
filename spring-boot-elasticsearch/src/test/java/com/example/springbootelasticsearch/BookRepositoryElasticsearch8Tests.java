7fed8b08ddc41fd591e17c7ccd617da08128629d
Eddú Meléndez <eddu.melendez@gmail.com>
2025-02-25 15:30:17 -0600

    Use @Ssl and @ServiceConnection for Elasticseach 8

--- spring-boot-elasticsearch/src/test/java/com/example/springbootelasticsearch/BookRepositoryElasticsearch8Tests.java
+++ spring-boot-elasticsearch/src/test/java/com/example/springbootelasticsearch/BookRepositoryElasticsearch8Tests.java
@@ -1,16 +1,11 @@
 package com.example.springbootelasticsearch;
 
-import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
-import org.elasticsearch.client.RestClientBuilder;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.elasticsearch.RestClientBuilderCustomizer;
 import org.springframework.boot.test.autoconfigure.data.elasticsearch.DataElasticsearchTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.boot.testcontainers.service.connection.Ssl;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchTemplate;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.elasticsearch.ElasticsearchContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -19,19 +14,15 @@ import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@DataElasticsearchTest(
-		properties = { "spring.elasticsearch.username=elastic", "spring.elasticsearch.password=changeme" })
+@DataElasticsearchTest
 @Testcontainers
 class BookRepositoryElasticsearch8Tests {
 
 	@Container
+	@ServiceConnection
+	@Ssl
 	private static ElasticsearchContainer elasticsearch = new ElasticsearchContainer(
 			"docker.elastic.co/elasticsearch/elasticsearch:8.7.1");
-
-	@DynamicPropertySource
-	static void properties(DynamicPropertyRegistry registry) {
-		registry.add("spring.elasticsearch.uris", () -> "https://" + elasticsearch.getHttpHostAddress());
-	}
 
 	@Autowired
 	private BookRepository bookRepository;
@@ -45,26 +36,6 @@ class BookRepositoryElasticsearch8Tests {
 		this.bookRepository.save(new Book(id, "Spring Boot Testing"));
 
 		assertThat(this.elasticsearchTemplate.get(id, Book.class)).extracting("title").isEqualTo("Spring Boot Testing");
-	}
-
-	@TestConfiguration
-	static class SSL {
-
-		@Bean
-		public RestClientBuilderCustomizer customizer() {
-			return new RestClientBuilderCustomizer() {
-				@Override
-				public void customize(RestClientBuilder builder) {
-
-				}
-
-				@Override
-				public void customize(HttpAsyncClientBuilder builder) {
-					builder.setSSLContext(elasticsearch.createSslContextFromCa());
-				}
-			};
-		}
-
 	}
 
 }
